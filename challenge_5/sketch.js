a027d8872c618b1cb0c1663774dbca919c5b785d
brinnathomsen <thomb212@gmail.com>
2015-10-02 18:15:27 -0400

    Challenge 5 Perfected

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,3 +1,6 @@
+// Draw a circle that follows the mouse until the mouse is clicked and it freezes. A new circle then follows the mouse while being attached to the first with a line.
+
+// Remember ball X and Y positions
 var startX = 0
 var startY = 0
 
@@ -9,22 +12,27 @@ function setup() {
 
 function draw() {
 
+	// Define characteristics of background and ball
+
 	background(50, 10, 10);
 	strokeWeight(10);
-	ellipseMode(RADIUS);
-
 	fill(0, 100, 255);
 	stroke(255, 255, 255);
 
+	// Make the ball freeze when clicked
 
 	if (!isMousePressed) {
 		startX = mouseX;
 		startY = mouseY;
+
 	} else {
+		// Draw second ellipse and line when clicked
 		line(startX, startY, mouseX, mouseY);
-		ellipse(startX, startY, 20, 20);
+		ellipse(startX, startY, 50, 50);
 	}
 
-	ellipse(mouseX, mouseY, 20, 20);
+	ellipse(mouseX, mouseY, 50, 50);
 
 }
+
+// Lesson learned: 1 - variable allow for memory
