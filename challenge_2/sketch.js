8cd8d9e69ef9f5eb8b516822e0de944f20329cbb
brinnathomsen <thomb212@gmail.com>
2015-10-05 18:20:13 -0400

    Fixed function updateBall, which for some reason I tried to redo myself last week instead of fixing the original code

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -1,7 +1,5 @@
 // ball: a data object to hold info about the ball
 
-
-
 var ball = {
 	x: 500,
 	y: 100,
@@ -21,18 +19,17 @@ var paddle = {
 
 
 
-//// setup: called by p5 once at startup
+// setup: called by p5 once at startup
 function setup() {
 	createCanvas(640, 480);
 	noStroke();
 }
 
-
+// draw: called by p5 repeatedly to draw the game
 function draw() {
 
-
-
 	// paddle follows mouse
+	updateBall();
 	paddle.x = mouseX;
 
 
@@ -44,7 +41,9 @@ function draw() {
 	ellipse(ball.x, ball.y, ball.radius, ball.radius);
 	rect(paddle.x - paddle.width, paddle.y, paddle.width, paddle.height);
 
+}
 
+function updateBall() {
 
 	// change position
 	ball.x = ball.x + ball.speedX;
@@ -54,34 +53,32 @@ function draw() {
 	// don't let the ball go through the walls
 
 	// right
-	if (ball.x > 640) {
+	if (ball.x > width - ball.radius) {
 		ball.speedX = -ball.speedX;
 	}
 
 	// bottom
-	if (ball.y > 480) {
+	if (ball.y > height - ball.radius) {
 		ball.speedY = -ball.speedY;
 	}
 
 	// left
-	if (ball.x < 0) {
+	if (ball.x < 0 + ball.radius) {
 		ball.speedX = -ball.speedX;
 	}
 
 	// top
-	if (ball.y < 0) {
+	if (ball.y < 0 + ball.radius) {
 		ball.speedY = -ball.speedY;
 	}
 
-	// making the ball bounce off the paddle
+	// check for a *collision* between the ball and the paddle
+	// look up the && operator
 
-	if ((ball.x > paddle.x - paddle.width) && (ball.y > paddle.y)) {
-		ball.speedY = -ball.speedY;
+	if (ball.x > (paddle.x - paddle.width * 0.5) && ball.x < (paddle.x + paddle.width *
+			0.5) && ball.y > paddle.y - ball.radius) {
+		if (ball.speedY > 0) {
+			ball.speedY = -ball.speedY;
+		}
 	}
-
-	if ((ball.x > paddle.x + paddle.width) && (ball.y > paddle.y)) {
-		ball.speedY = -ball.speedY;
-	}
-
-
 }
